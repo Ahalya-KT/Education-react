b95ff67f4835d68dac9f8d5fd35026d03ae67312
Ahalya-KT <ahalyakt002@gmail.com>
2023-10-17 19:47:45 +0530

    edu

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react' 
+import { Link } from 'react-scroll'
+
+const Navbar = () => {
+  return (
+    <div>
+     
+     <div>
+        <div>
+            <Link to="/"> 
+                eStudy
+            </Link>
+        </div>
+        <nav>
+           <Link to="home" spy={true} smooth={true} duration={500}>Home</Link>
+           <Link to="about" spy={true} smooth={true} duration={500}>About</Link>
+           <Link to="courses" spy={true} smooth={true} duration={500}>Courses</Link>
+           <Link to="reviews" spy={true} smooth={true} duration={500}>Reviews</Link>
+           <Link to="Contact" spy={true} smooth={true} duration={500}>Contact</Link>
+        </nav>
+     </div>
+    </div>
+  )
+}
+
+export default Navbar
